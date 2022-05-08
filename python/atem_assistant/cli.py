89be41e452bcfe387d574aba686bc70a753ea5c0
Ryo Takahashi <takahashi@stu-inc.com>
2022-05-08 22:45:28 +0900

    実用的なところまで実装して少し整頓

--- python/atem_assistant/cli.py
+++ python/atem_assistant/cli.py
@@ -3,30 +3,18 @@
 
 from __future__ import absolute_import, division, print_function
 
-import os.path
 import logging
-
-import httplib2
-import os
+import os.path
 import sys
 
-import pickle
-import os.path
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
-
-'''
-from apiclient.discovery import build
-from apiclient.errors import HttpError
-from oauth2client.client import flow_from_clientsecrets
-from oauth2client.file import Storage
-from oauth2client.tools import argparser, run_flow
-'''
-
-API_SERVICE_NAME = "youtube"
-API_VERSION = "v3"
-SCOPES = ["https://www.googleapis.com/auth/youtube"]
+from atem_assistant.google import (
+    auth,
+    upload_thumbnail,
+    print_categories,
+    print_playlists,
+    insert_broadcast,
+    insert_stream
+)
 
 logger = logging.getLogger("atem_assistant")
 
@@ -35,154 +23,6 @@ def this_dir(filename=""):
     path = os.path.dirname(__file__)
 
     return os.path.normpath(os.path.abspath(os.path.join(path, filename)))
-
-
-def auth(client_secret, token_file="token.pickle"):
-    creds = None
-
-    if os.path.exists(token_file):
-        with open(token_file, 'rb') as token:
-            creds = pickle.load(token)
-
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                client_secret, SCOPES)
-            creds = flow.run_local_server(port=0)
-
-        # Save the credentials for the next run
-        with open(token_file, 'wb') as token:
-            pickle.dump(creds, token)
-
-    return build(API_SERVICE_NAME, API_VERSION, credentials=creds)
-
-
-def upload_thumbnail(youtube, video_id, file):
-    response = youtube.thumbnails().set(
-        videoId=video_id,
-        media_body=file
-    ).execute()
-
-    return response
-
-
-def print_categories(youtube):
-    response = youtube.videoCategories().list(
-        part="id,snippet",
-        regionCode="JP"
-    ).execute()
-
-    logger.info("Categories:")
-    for item in response["items"]:
-        logger.info("{0} : {1}".format(item["id"], item["snippet"]["title"]))
-
-    return response
-
-
-def print_playlists(youtube):
-    response = youtube.playlists().list(
-        part="id,snippet,status",
-        mine=True
-    ).execute()
-
-    logger.info("My Playlists:")
-    for item in response["items"]:
-        logger.info("{0} : {1}".format(item["id"], item["snippet"]["title"]))
-
-    return response
-
-
-def add_to_playlist(youtube, playlist_id, video):
-    logger.debug(video)
-
-    response = youtube.playlistItems().insert(
-        part="snippet",
-        body=dict(
-            snippet=dict(
-                playlistId=playlist_id,
-                resourceId=video,
-            ),
-        )
-    ).execute()
-
-    return response
-
-
-# Create a liveBroadcast resource and set its title, scheduled start time,
-# scheduled end time, and privacy status.
-def insert_broadcast(
-        youtube, broadcast_title, description, start_time, privacy_status
-):
-    insert_broadcast_response = youtube.liveBroadcasts().insert(
-        part="snippet,status,contentDetails",
-        body=dict(
-            snippet=dict(
-                title=broadcast_title,
-                description=description,
-                scheduledStartTime=start_time,
-            ),
-            status=dict(
-                privacyStatus=privacy_status,
-                selfDeclaredMadeForKids=False
-            ),
-            contentDetails=dict(
-                enableAutoStart=True,
-                enableAutoStop=True,
-            )
-        )
-    ).execute()
-
-    snippet = insert_broadcast_response["snippet"]
-
-    logger.info("Broadcast '{0}' with title '{1}' was published at '{2}'.".format(
-        insert_broadcast_response["id"], snippet["title"], snippet["publishedAt"]))
-
-    return insert_broadcast_response["id"]
-
-
-# Create a liveStream resource and set its title, format, and ingestion type.
-# This resource describes the content that you are transmitting to YouTube.
-def insert_stream(youtube, broadcast_id, stream_title):
-    response = youtube.liveStreams().insert(
-        part="snippet,cdn",
-        body=dict(
-            snippet=dict(
-                title=stream_title
-            ),
-            cdn=dict(
-                ingestionType="rtmp",
-                resolution="1080p",
-                frameRate="60fps",
-            )
-        )
-    ).execute()
-
-    snippet = response["snippet"]
-
-    logger.info("Stream '{0}' with title '{1}' was inserted.".format(
-        response["id"], snippet["title"]))
-
-    bind_broadcast(youtube, broadcast_id, response["id"])
-
-    return response["cdn"]["ingestionInfo"]["streamName"]
-
-
-# Bind the broadcast to the video stream. By doing so, you link the video that
-# you will transmit to YouTube to the broadcast that the video is for.
-def bind_broadcast(youtube, broadcast_id, stream_id):
-    bind_broadcast_response = youtube.liveBroadcasts().bind(
-        part="id,contentDetails",
-        id=broadcast_id,
-        streamId=stream_id
-    ).execute()
-
-    logger.info("Broadcast '{0}' was bound to stream '{1}'.".format(
-        bind_broadcast_response["id"],
-        bind_broadcast_response["contentDetails"]["boundStreamId"])
-    )
 
 
 def create_youtube_live(
@@ -200,11 +40,10 @@ def create_youtube_live(
     broadcast_id = insert_broadcast(youtube, broadcast_title, description, start_time,
                                     privacy_status)
     stream_name = insert_stream(youtube, broadcast_id, stream_title)
-    logger.info("stream: {0}".format(stream_name))
 
     upload_thumbnail(youtube, broadcast_id, thumbnail)
 
-    # add_to_playlist(youtube, playlist_id, broadcast_id)
+    return stream_name
 
 
 def main():
@@ -282,7 +121,7 @@ def main():
     _logger.setLevel(_log_level)
 
     # Create YouTube live stream
-    create_youtube_live(
+    stream_name = create_youtube_live(
         args.client_secret,
         args.broadcast_title, args.broadcast_description,
         args.start_time, args.privacy_status,
@@ -290,6 +129,7 @@ def main():
         args.thumbnail,
         args.playlist_id, args.category,
     )
+    logger.info("stream: {0}".format(stream_name))
 
     # Upload ATEM setting
     pass
