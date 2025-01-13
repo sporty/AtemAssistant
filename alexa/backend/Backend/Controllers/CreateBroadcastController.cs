8df5e99cf12f9823597be57dd4a019d7f35f7a68
Ryo Takahashi <rt.sport@gmail.com>
2025-01-13 12:57:14 +0900

    実行ファイルパスを取得してLinuxでもエラーにならないようにした

--- alexa/backend/Backend/Controllers/CreateBroadcastController.cs
+++ alexa/backend/Backend/Controllers/CreateBroadcastController.cs
@@ -12,12 +12,15 @@ public class CreateBroadcastController : ControllerBase
     {
         try
         {
+            string executeFilePath = Process.GetCurrentProcess().MainModule.FileName;
+            Console.WriteLine($"Execute '{executeFilePath}' to create broadcast.");
+
             // Backend.exeコマンドを実行
             var process = new Process()
             {
                 StartInfo = new ProcessStartInfo()
                 {
-                    FileName = "Backend.exe",
+                    FileName = executeFilePath,
                     Arguments = "createBroadcast",
                     RedirectStandardOutput = true,
                     UseShellExecute = false,
